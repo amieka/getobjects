b000d02e651e33503f035c65fd4585ea3e19f9fe
helge.hess <helge.hess@8aca4512-4549-0410-9768-61877113ad56>
2008-04-10 09:24:02 +0000

    tagged Go 1.5.2

---
+++ core/tags/1.5.2/org/getobjects/jsapp/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+JARDIR="$HOME/dev/eclipse-go/GETobjects/ThirdParty"
+GOPATH="$HOME/dev/eclipse-go/GETobjects/bin"
+
+RUNNER="org.getobjects.jsapp.run"
+WOPORT=5859
+
+# ThirdParty dependencies
+CLASSPATH="" # reset
+CLASSPATH="`ls ${JARDIR}/commons-fileupload-1.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/commons-logging-1.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/javax.servlet.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/jetty-6.1.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/jetty-util-6.1.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/log4j-1.2.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/ognl-2.6.*.jar`:${CLASSPATH}"
+CLASSPATH="`ls ${JARDIR}/js*.jar`:${CLASSPATH}"
+
+# JOPE
+CLASSPATH="${GOPATH}:${CLASSPATH}"
+
+echo "CLASSPATH: ${CLASSPATH}"
+
+# run the runner ;-)
+java -cp ".:${CLASSPATH}" "${RUNNER}" . -DWOPort=${WOPORT}
